22d9bfc75b23979405bdcfd0e6852134dba5d71f
Sai <Sai@DESKTOP-5HI687G>
2018-11-09 19:58:50 -0500

    babel src webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,48 @@
+/*
+    ./webpack.config.js
+*/
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: './public/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})
+
+
+
 module.exports = {
-  entry: path.resolve(__dirname, 'example/main.jsx'),
-  output: {
-    path: path.resolve(__dirname, 'example'),
-    publicPath: '/',
-    filename: 'bundle.js',
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'example'),
-    host: '0.0.0.0',
-    disableHostCheck: true,
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        use: ['babel-loader'],
-      },
-    ],
-  },
-};
+    entry: './src/index.js',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" }
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            }, {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            },
+            {
+                test: /\.(ttf|eot|svg|gif|png|mp3)(\?v=[0-9]\.[0-9]\.[0-9])?$/,                
+                use: [{
+                    loader: 'file-loader'
+                }]
+            }
+        ]
+    },
+    plugins: [HtmlWebpackPluginConfig]
+}
